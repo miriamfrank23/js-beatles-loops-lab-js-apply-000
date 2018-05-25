6469a35b83eeb844e305201956ec7045eedc09ae
Miriam Frank <miriamfrank23@gmail.com>
2018-05-25 01:22:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay (array1, array2) {
   let array3 = [];
   for (let i = 0; i < array1.length; i++) {
-    array3.push(array1[i] + array2[i]);
+    array3.push(array1[i] + " plays " + array2[i]);
   }
   return array3;
 }
